73e09725e88f2f8a6dd9bb014853d824fd9f4535
Luciany17 <102994126+Luciany17@users.noreply.github.com>
2022-04-05 03:50:57 +0800

    pull commit

--- src/test/java/SpringTest.java
+++ src/test/java/SpringTest.java
@@ -16,6 +16,7 @@ public class SpringTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 
 }
